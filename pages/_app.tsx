faf432f8341f9aefaa1dab70f5984db678f29b81
Nguyen Hieu <tthzicc@gmail.com>
2021-09-26 09:38:16 +0700

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
   return <Component {...pageProps} />
 }
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
+  console.log(__dirname, 'aaa')
   return (
     <div className={styles.container}>
       <Head>
@@ -14,12 +15,12 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          CONGURATIONS!!!! :V
+          {/* Welcome to <a href="https://nextjs.org">Next.js!</a> */}
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+         BUILDED SUCCESSFULY
         </p>
 
         <div className={styles.grid}>
